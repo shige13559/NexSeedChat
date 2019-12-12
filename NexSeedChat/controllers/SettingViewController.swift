e05ba0f66243aa161d29d2d5cccd3d04cb802fe4
原田茂大 <sapori946@gmail.com>
2019-12-12 14:28:18 +0800

    設定画面のレイアウト配置・ホーム画面の場所固定・文の背景色変更・settingViewController作成・アイコンの表示

---
+++ NexSeedChat/controllers/SettingViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SettingViewController.swift
+//  NexSeedChat
+//
+//  Created by 原田茂大 on 2019/12/12.
+//  Copyright © 2019 geshi. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SettingViewController: UIViewController {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
